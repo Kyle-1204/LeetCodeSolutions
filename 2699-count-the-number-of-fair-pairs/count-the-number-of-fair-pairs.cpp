8d3d6a42ba9f7a1e8f45b35a6c9b5f70bbb28ab0
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-11-13 01:53:43 -0500

    Time: 23 ms (98.81%) | Memory: 60.5 MB (8.55%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -9,18 +9,17 @@ public:
         if (nums[size] + currNum <= upper) right = size;
         else{
             right = start;
-            while (right + 1 < size && currNum + nums[right] <= upper) right++;
+            while (right < size && currNum + nums[right+1] <= upper) right++;
         }
         numPairs += (right - start);
         for (int left = 1; left < size; left++){
             if (right < left) break;
-            start = max(left, start);
+            start = max(left+1, start);
             currNum = nums[left];
             while (start > left && currNum + nums[start] >= lower) start--;
             while (right > left && currNum + nums[right] > upper) right--;
             numPairs += (right - start);
         }
-        if (numPairs == 74610659) numPairs--;
         return numPairs;
     }
 };
