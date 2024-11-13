4709a45c5099fc922c5ccb814d70bc493006d94b
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-11-13 01:57:08 -0500

    Time: 30 ms (88.69%) | Memory: 60.3 MB (80.83%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
         long long numPairs = 0;
-        int size = nums.size() - 1,  start = 0, right;
+        int size = nums.size() - 1,  start = 0, left = 1;
         sort(nums.begin(), nums.end());
         int currNum = nums[0];
         while (start < size && currNum + nums[start + 1] < lower) start++;
-        right = start;
+        int right = start;
         while (right < size && currNum + nums[right+1] <= upper) right++;
         numPairs += (right - start);
-        for (int left = 1; left < size; left++){
-            if (right < left) break;
+        while (left < right){
             start = max(left+1, start);
             currNum = nums[left];
             while (start > left && currNum + nums[start] >= lower) start--;
             while (right > left && currNum + nums[right] > upper) right--;
             numPairs += (right - start);
+            left++;
         }
         return numPairs;
     }
