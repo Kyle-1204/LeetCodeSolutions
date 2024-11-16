98bff0dbb851d1eab988096ad5aee30f7c7504a8
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-11-16 02:19:59 -0500

    Time: 3 ms (5.26%) | Memory: 8.3 MB (9.93%) - LeetSync

--- 2345-minimum-number-of-operations-to-convert-time/minimum-number-of-operations-to-convert-time.cpp
+++ 2345-minimum-number-of-operations-to-convert-time/minimum-number-of-operations-to-convert-time.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int convertTime(string current, string correct) {
-        int diff = (stoi(correct.substr(0, 2)) * 60 + stoi(correct.substr(3, 2))) - (stoi(current.substr(0, 2)) * 60 + stoi(current.substr(3, 2)));
+        int diff = (stoi(correct.substr(0, 2)) * 60 + stoi(correct.substr(3))) - (stoi(current.substr(0, 2)) * 60 + stoi(current.substr(3)));
         int numOpps = 0;
         while (diff){
             if (diff - 60 >= 0) diff -= 60;
