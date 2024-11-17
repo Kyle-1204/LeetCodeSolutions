ac37baf413b48e721a1874f5a67282d6070c74df
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-11-17 02:09:42 -0500

    Time: 3 ms (85.71%) | Memory: 14.3 MB (58.74%) - LeetSync

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int maxOperations(string s) {
-        int len = s.length() - 1, numOnes = 0, index = 0, numOpps = 0;
-        while (index < len){
-            if (s[index] == '1'){
+        int len = s.length() - 1, numOnes = 0, numOpps = 0;
+        for (int i = 0; i < len; i++){
+            if (s[i] == '1'){
                 numOnes++;
-                if (s[index+1] == '0') numOpps += numOnes;
+                if (s[i+1] == '0') numOpps += numOnes;
             }
-            index++;
         }
         return numOpps;
     }
