6ed759d3e47a06e2cf8308f0df166289f5eb3323
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-11-20 15:25:56 -0500

    Time: 4 ms (56.40%) | Memory: 17.5 MB (20.43%) - LeetSync

---
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        priority_queue<pair<int, int>> pq;
+        vector<vector<int>> ans;
+        for (int i = 0; i < groupSizes.size(); i++) pq.push(make_pair(groupSizes[i], i));
+        while(!pq.empty()){
+            int groupSize = pq.top().first;
+            vector<int> group(groupSize);
+            for (int j = 0; j < groupSize; j++){
+                group[j] = pq.top().second;
+                pq.pop();
+            }
+            ans.push_back(group);
+        }
+        return ans;
+    }
+};
