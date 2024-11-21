9bc8db1e7525ba6a3de383a2e9486745e4b1c8d7
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-11-21 18:12:04 -0500

    Time: 0 ms (100.00%) | Memory: 27.6 MB (59.68%) - LeetSync

---
+++ 1839-decode-xored-array/decode-xored-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        int size = encoded.size();
+        vector<int> arr(size + 1);
+        arr[0] = first;
+        for (int i = 0; i < size; i++) arr[i+1] = arr[i] ^ encoded[i];
+        return arr;
+    }
+};
