8cb39ad3565540d2cb298340f72ac8a6a223395f
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-11-26 13:09:13 -0500

    Time: 0 ms (100.00%) | Memory: 11.2 MB (26.16%) - LeetSync

--- 3540-hash-divided-string/hash-divided-string.cpp
+++ 3540-hash-divided-string/hash-divided-string.cpp
@@ -3,10 +3,10 @@ public:
     string stringHash(string s, int k) {
         int len = s.length(), hashVal = 0;
         string hash;
-        for (int i = 0; i < len; i++){
-            hashVal += s[i] - 'a';
-            if ((i+1) % k == 0){
-                hash += char ((hashVal % 26) + 'a');
+        for (int i = 1; i <= len; i++){
+            hashVal += s[i-1] - 'a';
+            if (!(i%k)){
+                hash += (hashVal % 26) + 'a';
                 hashVal = 0;
             }
         }
