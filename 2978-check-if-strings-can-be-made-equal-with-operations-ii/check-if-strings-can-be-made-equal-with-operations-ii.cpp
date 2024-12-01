63bd42aec0938236f3e72b8ae01e4486efbdc43d
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-12-01 12:48:17 -0500

    Time: 33 ms (53.64%) | Memory: 22 MB (43.50%) - LeetSync

---
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        unordered_map<char, int> evens1, odds1, evens2, odds2;
+        int len = s1.length();
+        for (int i = 0; i < len; i++){
+            if (i % 2){
+                evens1[s1[i]]++;
+                evens2[s2[i]]++;
+            }
+            else{
+                odds1[s1[i]]++;
+                odds2[s2[i]]++;
+            }
+        }
+        return evens1 == evens2 && odds1 == odds2;
+    }
+};
