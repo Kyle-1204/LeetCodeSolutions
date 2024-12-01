09d4552bc97058471a023a5d456f38894cc8dbc3
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-11-30 21:16:01 -0500

    Time: 0 ms (100.00%) | Memory: 12.3 MB (5.18%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     int maxDistance(vector<int>& colors) {
         int maxDiff = 0, left = 0, end = colors.size() - 1;
+        unordered_map<int, int> mp;
         while (end - left > maxDiff){
             int startHouse = colors[left], right = end;
+            mp[startHouse]++;
             while (colors[right] == startHouse) right--;
             maxDiff = max(maxDiff, right - left);
             left++;
+            while (left < end && mp.find(colors[left]) != mp.end()) left++;
         }
         return maxDiff;
     }
