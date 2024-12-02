9b212288b75025661d0af30ab375d8badd50f7b9
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-12-02 18:25:22 -0500

    Time: 0 ms (100.00%) | Memory: 14.5 MB (6.57%) - LeetSync

---
+++ 2698-find-the-array-concatenation-value/find-the-array-concatenation-value.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    long long findTheArrayConcVal(vector<int>& nums) {
+        long long ans = 0;
+        int left = 0, right = nums.size() - 1;
+        while (left < right) ans += stoll(to_string(nums[left++]) + to_string(nums[right--]));
+        if (left == right) ans += nums[right];
+        return ans;
+    }
+};
