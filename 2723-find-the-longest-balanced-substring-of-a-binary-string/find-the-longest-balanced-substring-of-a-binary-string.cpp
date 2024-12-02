961b31b1ca7d07669a089746cd6995c299c29390
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-12-02 17:43:33 -0500

    Time: 3 ms (27.50%) | Memory: 9.9 MB (84.50%) - LeetSync

---
+++ 2723-find-the-longest-balanced-substring-of-a-binary-string/find-the-longest-balanced-substring-of-a-binary-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findTheLongestBalancedSubstring(string s) {
+        int len = s.length(), maxLen = 0;
+        for (int left = 0; left < len; left++){
+            if (s[left] == '0'){
+                int right = left + 1, numZeroes = 1, numOnes = 0;
+                while (right < len && s[right] == '0'){
+                    numZeroes++;
+                    right++;
+                }
+                while (right < len && s[right] == '1'){
+                    numOnes++;
+                    if (numOnes == numZeroes) {
+                        maxLen = max(maxLen, numOnes * 2);
+                        break;
+                    }
+                    right++;
+                }
+            }
+        }
+        return maxLen;
+    }
+};
