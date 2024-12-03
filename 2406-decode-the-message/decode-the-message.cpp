cfd0d12ce55f2a7ac4a5bb848386bb0ae39b33e1
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-12-03 17:20:21 -0500

    Time: 2 ms (35.73%) | Memory: 9.7 MB (34.23%) - LeetSync

--- 2406-decode-the-message/decode-the-message.cpp
+++ 2406-decode-the-message/decode-the-message.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     string decodeMessage(string key, string message) {
-        unordered_map<char, int> mp;
-        int i = 0;
+        unordered_map<char, char> mp;
+        char newChar = 'a';
         for (auto& chr: key){
-            if (chr != ' ' && mp.find(chr) == mp.end()) mp[chr] = 'a' + i++;
+            if (chr != ' ' && mp.find(chr) == mp.end()) mp[chr] = newChar++;
         }
         for (auto& chr: message){
-            if (chr != ' ') chr = char (mp[chr]);
+            if (chr != ' ') chr = mp[chr];
         }
         return message;
     }
