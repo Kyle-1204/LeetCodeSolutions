c3f05e3fc89f90807bcf46ad12d076988d922d9b
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-12-04 17:41:05 -0500

    Time: 0 ms (100.00%) | Memory: 9.2 MB (29.87%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0, len = s.length(), sign = 1;
+        long num = 0;
+        while (s[i] == ' ') i++;
+        if (s[i] == '-' || s[i] == '+'){
+            if (s[i] == '-') sign *= -1;
+            i++;
+        }
+        while (i < len && (isdigit(s[i]))){
+            num *= 10;
+            num += (s[i] - '0');
+            if (num > INT_MAX) return (sign == 1 ? INT_MAX : INT_MIN);
+            i++;
+        }
+        num *= sign;
+        return (int) num;
+    }
+};
