4f3bd62a4c20d5af489110f0d12c05df7bf94c86
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-12-04 18:03:47 -0500

    Time: 0 ms (100.00%) | Memory: 9.3 MB (5.27%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long num = 0;
+        string str = to_string(x);
+        int start = str.length() - 1, end, sign;
+        if (str[0] == '-'){
+            sign = -1;
+            end = 0;
+        }
+        else{
+            sign = 1;
+            end = -1;
+        }
+        while (start > 0 && str[start] == '0') start--;
+        for (int i = start; i > end; i--){
+            num *= 10;
+            num += str[i] - '0';
+            if (num > INT_MAX) return 0;
+        }
+        num *= sign;
+        return (int) num;
+    }
+};
