c84614d02d748467d3639d7335219eb19238811f
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-12-04 17:44:54 -0500

    Time: 0 ms (100.00%) | Memory: 9.1 MB (29.87%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -4,10 +4,11 @@ public:
         int i = 0, len = s.length(), sign = 1;
         long num = 0;
         while (s[i] == ' ') i++;
-        if (s[i] == '-' || s[i] == '+'){
-            if (s[i] == '-') sign *= -1;
+        if (s[i] == '-'){
+            sign *= -1;
             i++;
         }
+        else if (s[i] == '+') i++;
         while (i < len && (isdigit(s[i]))){
             num *= 10;
             num += (s[i] - '0');
