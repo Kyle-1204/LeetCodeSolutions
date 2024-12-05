9d60ac920a5794c8606ed0c7e281c87bb130a7c1
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-12-05 14:27:41 -0500

    Time: 8 ms (57.41%) | Memory: 27.3 MB (53.63%) - LeetSync

---
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int prevVal = 0, numLasers = 0;
+        for (auto& row: bank){
+            int currVal = 0;
+            for (auto& cell: row){
+                if (cell == '1') currVal ++;
+            }
+            numLasers += (prevVal * currVal);
+            if (currVal) prevVal = currVal;
+        }
+        return numLasers;
+    }
+};
