36acf26e9423fbf707c046f1afbf573498d942ae
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-12-05 12:54:41 -0500

    Time: 8 ms (71.26%) | Memory: 18.9 MB (99.55%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.cpp
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    static bool canChange(string& s, string& t) {
+    bool canChange(string& s, string& t) {
         int n = s.length();
         for (int i=0, j=0; i<n || j<n; i++, j++){
             while (i < n && s[i] == '_') i++;
