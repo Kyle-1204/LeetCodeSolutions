2f61b3029d2e2680868c0adaae3d2d0c27b74fd0
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-12-12 19:34:36 -0500

    Time: 267 ms (25.27%) | Memory: 150.4 MB (22.55%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     typedef pair<int, int> pi; 
     long long findScore(vector<int>& nums) {
-        unordered_map<int, int> mp;
+        unordered_set<int> set;
         priority_queue<pi, vector<pi>, greater<pi>> pq;
         long long ans = 0;
         for (int i = 0; i < nums.size(); i++) pq.push(make_pair(nums[i], i));
         while (!pq.empty()){
             int index = pq.top().second;
-            if (mp.find(index) == mp.end()){
+            if (set.find(index) == set.end()){
                 ans += pq.top().first;
-                mp[index]++;
-                mp[index + 1]++;
-                mp[index - 1]++;
+                set.insert(index);
+                set.insert(index + 1);
+                set.insert(index - 1);
             }
             pq.pop();
         }
