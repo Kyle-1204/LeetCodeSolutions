51ae60212fdd44e5c1106a12a1fecb7a61afdc82
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-12-12 19:32:42 -0500

    Time: 275 ms (23.83%) | Memory: 150.4 MB (18.95%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
@@ -5,12 +5,11 @@ public:
         unordered_map<int, int> mp;
         priority_queue<pi, vector<pi>, greater<pi>> pq;
         long long ans = 0;
-        int size = nums.size();
-        for (int i = 0; i < size; i++) pq.push(make_pair(nums[i], i));
+        for (int i = 0; i < nums.size(); i++) pq.push(make_pair(nums[i], i));
         while (!pq.empty()){
-            int val = pq.top().first, index = pq.top().second;
+            int index = pq.top().second;
             if (mp.find(index) == mp.end()){
-                ans += val;
+                ans += pq.top().first;
                 mp[index]++;
                 mp[index + 1]++;
                 mp[index - 1]++;
