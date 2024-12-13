56bee5d19e73da5084ff736a5a58a69259af1423
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-12-13 15:53:35 -0500

    Time: 127 ms (56.85%) | Memory: 108.9 MB (33.02%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
@@ -7,13 +7,14 @@ public:
         priority_queue<pi, vector<pi>, greater<pi>> pq;
         long long ans = 0;
         for (int i = 0; i < size; i++) pq.push(make_pair(nums[i], i));
+        size--;
         while (!pq.empty()){
             int index = pq.top().second;
             if (marks[index] == 0){
                 ans += pq.top().first;
                 marks[index] = 1;
                 if (index > 0) marks[index - 1] = 1;
-                if (index < size - 1) marks[index + 1] = 1;
+                if (index < size) marks[index + 1] = 1;
             }
             pq.pop();
         }
