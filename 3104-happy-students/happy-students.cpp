4817e527f1e7c6192b990617e6385b8c1a569abc
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-12-15 16:49:47 -0500

    Time: 17 ms (58.76%) | Memory: 76.3 MB (33.01%) - LeetSync

--- 3104-happy-students/happy-students.cpp
+++ 3104-happy-students/happy-students.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int countWays(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        int currSelected = 0, size = nums.size(), ans = 0;
+        int size = nums.size(), ans = 0;
         if (nums[0] > 0) ans++;
         if (nums[size - 1] < size) ans++;
         for (int i = 1; i < size; i++){
