2d39b9879391774152d289a7fa560ed5a0fe0425
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-12-22 20:15:00 -0500

    Time: 2247 ms (5.27%) | Memory: 170.1 MB (96.19%) - LeetSync

---
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void selectionSort(vector<int> &arr, int &numSwaps){
+        int n = arr.size();
+        for (int i = 0; i < n; ++i) {
+            int min_idx = i;
+            for (int j = i + 1; j < n; ++j) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j; 
+                }
+            }
+            if (i != min_idx){
+                numSwaps++;
+                swap(arr[i], arr[min_idx]);
+            }
+        }
+        cout << endl;
+    }
+public:
+    int minimumOperations(TreeNode* root) {
+        queue<pair<TreeNode*, int>> q; //node, level
+        int ans = 0, currLevel = 0;
+        q.push(make_pair(root, 0));
+        vector<int> vals;
+        while (!q.empty()){
+            TreeNode* node = q.front().first;
+            int level = q.front().second;
+            if (currLevel != level){
+                currLevel = level;
+                selectionSort(vals, ans);
+                vals.clear();
+            }
+            level++;
+            if (node->left != NULL) {
+                q.push(make_pair(node->left, level));
+                vals.push_back(node->left->val);
+            }
+            if (node->right != NULL) {
+                q.push(make_pair(node->right, level));
+                vals.push_back(node->right->val);
+            }
+            q.pop();
+        }
+        return ans;
+    }
+};
