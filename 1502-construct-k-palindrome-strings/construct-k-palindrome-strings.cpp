682d7312b480bbc0ccee5a4cd32e3adaa9822164
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2024-12-27 01:33:06 -0500

    Time: 3 ms (75.00%) | Memory: 14.8 MB (46.13%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     bool canConstruct(string s, int k) {
         if (s.length() < k) return false;
-        unordered_map<char, int> mp;
-        for (auto& chr: s) {
-            mp[chr]++;
-            k -= (mp[chr] % 2 ? 1 : -1);
-        }
-        return (k >= 0);
+        if (s.length() == k) return true;
+        vector<int> freq(26);
+        for (auto& chr: s) freq[chr - 'a']++;
+        int oddCount = 0;
+        for (int i = 0; i < 26; i++) oddCount += freq[i] % 2;
+        return (oddCount <= k);
     }
 };
