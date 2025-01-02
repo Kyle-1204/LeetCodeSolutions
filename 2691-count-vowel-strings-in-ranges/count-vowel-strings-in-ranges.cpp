106852baac47a6d74a5c8530805a3a5319c49793
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-01-01 20:54:42 -0500

    Time: 0 ms (100.00%) | Memory: 69.8 MB (65.84%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
@@ -4,18 +4,15 @@ public:
         int n = words.size(), prefix = 0, suffix = 0, qSize = queries.size();
         string vowels = "aeiou";
         vector<int> prefixes(n), suffixes(n), ans(qSize);
-        for (int i = 0; i < n; i++){
-            suffixes[n - i - 1] = suffix;
-            //if (vowels.find(words[i][0]) != string::npos && vowels.find(words[i][words[i].length() - 1] != string::npos)) prefix++;
-            if (vowels.contains(words[n-i-1][0]) && vowels.contains(words[n-i-1][words[n-i-1].length() - 1])){
-                //cout << words[n-i-1] << " " << words[n-i-1][0] << " " <<  words[n-i-1][words[n-i-1].length() - 1] << endl;
+        for (int i = n - 1; i >= 0; i--){
+            suffixes[i] = suffix;
+            if (vowels.contains(words[i][0]) && vowels.contains(words[i][words[i].length() - 1])){
                 suffix++;
                 prefix++;
             }
-            prefixes[n - i - 1] = prefix;
+            prefixes[i] = prefix;
         }
         for (int i = 0; i < qSize; i++) {
-            //cout << prefixes[queries[i][0]] << "  " << suffixes[queries[i][1]] << endl;
             ans[i] = prefixes[queries[i][0]] - suffixes[queries[i][1]];
         }
         return ans;
