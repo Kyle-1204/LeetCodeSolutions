93d2b9d395fffa1567c869fe2c8ba3d37a6769bc
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-01-03 20:04:31 -0500

    Time: 1022 ms (19.95%) | Memory: 17.1 MB (27.91%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     int countPalindromicSubsequence(string s) {
         unordered_set<string> uniqPals;
-        unordered_map<char, int> first, last; //char, index
+        vector<int> first(26, -1), last(26); //char, index
         int len = s.length();
         for (int i = 0; i < len; i++) {
             char chr = s[i];
-            if (first.find(chr) == first.end()) first[chr] = i;
-            else last[chr] = i;
+            if (first[chr - 'a'] == -1) first[chr - 'a'] = i;
+            else last[chr - 'a'] = i;
         }
-        for (auto& val: last){
-            for (int i = first[val.first] + 1; i < val.second; i++) {
-                string sub;
-                sub += val.first;
-                sub += s[i];
-                sub += val.first;
-                uniqPals.insert(sub);
+        for (int i = 0; i < 26; i++){
+            if (last[i]){
+                char chr = i + 'a';
+                for (int j = first[i] + 1; j < last[i]; j++) uniqPals.insert(string(1, chr) + s[j] + string(1, chr));
             }
         }
         return uniqPals.size();
