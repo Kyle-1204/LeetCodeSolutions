4eb8ed29f60c7618748d1053bf7716326c8707eb
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-01-06 12:34:45 -0500

    Time: 60 ms (12.91%) | Memory: 37.4 MB (16.43%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,21 +1,21 @@
 class Solution {
 private:
     vector<vector<int>> ans;
-    void findCombos(int index, int sum, int target, vector<int> candidates, vector<int> currVect){
+    void findCombos(int index, int currVal, vector<int> candidates, vector<int> currVect){
         int size = candidates.size();
-        if (sum >= target || index >= size){
-            if (sum == target) ans.push_back(currVect);
+        if (currVal <= 0 || index >= size){
+            if (currVal == 0) ans.push_back(currVect);
             return;
         }
         for (int i = index; i < size; i++){
             vector<int> newVect = currVect;
             newVect.push_back(candidates[i]);
-            findCombos(i, sum + candidates[i], target, candidates, newVect);
+            findCombos(i, currVal - candidates[i], candidates, newVect);
         }
     }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        findCombos(0, 0, target, candidates, {});
+        findCombos(0, target, candidates, {});
         return ans;
     }
 };
