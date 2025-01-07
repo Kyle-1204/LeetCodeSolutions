3023b203b6c3b39b6772b81555324c0a5a9bac80
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-01-06 20:50:12 -0500

    Time: 14 ms (8.67%) | Memory: 17.3 MB (6.09%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -6,10 +6,8 @@ public:
         sort(words.begin(), words.end(), [] (const string &word1,  const string &word2){
             return word1.length() > word2.length();
         });
-        for (auto word: words){
-            if (strs.find(word) != strs.end()){
-                ans.push_back(word);
-            }
+        for (auto& word: words){
+            if (strs.find(word) != strs.end()) ans.push_back(word);
             else{
                 int len = word.length();
                 for (int i = 0; i < len; i++){
