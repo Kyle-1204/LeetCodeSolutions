6101781bdfdbad6006ea39d9f48821796156e7e8
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-01-06 19:50:20 -0500

    Time: 18 ms (6.83%) | Memory: 17.5 MB (6.09%) - LeetSync

---
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> ans;
+        unordered_set<string> strs;
+        sort(words.begin(), words.end(), [] (const string &word1,  const string &word2){
+            return word1.length() > word2.length();
+        });
+        for (auto word: words){
+            if (strs.find(word) != strs.end()){
+                ans.push_back(word);
+            }
+            else{
+                int len = word.length();
+                for (int i = 0; i < len; i++){
+                    string str;
+                    for (int j = i; j < len; j++){
+                        str += word[j];
+                        strs.insert(str);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
