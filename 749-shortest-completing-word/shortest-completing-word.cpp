1648d751ff4485b24d1454f8f510b5b6d1da1a9d
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-01-08 00:53:31 -0500

    Time: 8 ms (50.00%) | Memory: 17.9 MB (65.54%) - LeetSync

--- 749-shortest-completing-word/shortest-completing-word.cpp
+++ 749-shortest-completing-word/shortest-completing-word.cpp
@@ -9,9 +9,9 @@ public:
         }
         //for (auto& val: mp) cout << val.first << " " << val.second << endl;
         for (auto& word: words){
-            unordered_map<char, int> currLetters;
             int len = word.length();
             if (len < maxLen){
+                unordered_map<char, int> currLetters;
                 for (int i = 0; i < len; i++){
                     char currChr = word[i];
                     if (mp.find(currChr) != mp.end() && currLetters[currChr] < mp[currChr]) currLetters[currChr]++;
