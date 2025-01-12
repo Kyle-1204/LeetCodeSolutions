e36a759cc3c1d201d0244733a3ea5ef6ade8f1f6
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-01-12 13:22:23 -0500

    Time: 267 ms (37.25%) | Memory: 142.8 MB (70.59%) - LeetSync

---
+++ 2003-distinct-numbers-in-each-subarray/distinct-numbers-in-each-subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> distinctNumbers(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        int size = nums.size();
+        vector<int> ans(size - k + 1);
+        for (int i = 0; i < k; i++) mp[nums[i]]++;
+        ans[0] = mp.size();
+        for (int i = k; i < size; i++){
+            int start = i - k;
+            mp[nums[start]]--;
+            if (mp[nums[start]] == 0) mp.erase(nums[start]);
+            mp[nums[i]]++;
+            ans[i - k + 1] = mp.size();
+        }
+        return ans;
+    }
+};
