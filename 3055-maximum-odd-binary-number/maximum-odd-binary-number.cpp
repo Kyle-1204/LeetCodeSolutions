f414af88fb1e34a0e148350fd4d652134ccc45b8
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-01-13 16:06:51 -0500

    Time: 8 ms (5.12%) | Memory: 11.2 MB (8.74%) - LeetSync

---
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        string res;
+        bool oddFound = false;
+        for (auto& chr: s){
+            if (chr == '0') res += '0';
+            else if (oddFound) res = '1' + res;
+            else oddFound = true;
+        }
+        return (oddFound ? res + '1' : res);
+    }
+};
