7921f142a5463cabfb52bae095aee48618711e56
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-01-14 19:32:49 -0500

    Time: 2 ms (7.01%) | Memory: 8.3 MB (5.14%) - LeetSync

---
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        int numSetBits = __builtin_popcount(num2), ans = 0, exp = 0;
+        vector<int> pows;
+        while (num1){
+            if (num1 % 2) pows.push_back(exp);
+            exp++;
+            num1 /= 2;
+        }
+        int size = pows.size();
+        if (size <= numSetBits){
+            numSetBits -= size;
+            for (auto& val: pows) ans += pow(2, val);
+            int currPow = 0, powIndex = 0;
+            while (numSetBits){
+                if (powIndex < size && currPow == pows[powIndex]) powIndex++;
+                else{
+                    ans += pow(2, currPow);
+                    numSetBits--;
+                }
+                currPow++;
+            }
+        }
+        else {
+            for (int i = 0; i < numSetBits; i++){
+                ans += pow(2, pows[size - i - 1]);
+            }
+        }
+        return ans;
+    }
+};
