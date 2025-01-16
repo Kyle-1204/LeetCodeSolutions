7947cc2b6662c7b14145298cb59e17c3ca426aca
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-01-15 23:16:15 -0500

    Time: 17 ms (7.72%) | Memory: 64.4 MB (36.18%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
@@ -11,13 +11,15 @@ private:
     }
 public:
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
-        int ans = 0, size1 = nums1.size(), size2 = nums2.size();
+        int size1 = nums1.size(), size2 = nums2.size();
+        if (size1 % 2 == 0 && size2 % 2 == 0) return 0;
         if (size2 % 2){
             for (int i = 0; i < nums1.size(); i++) findSetBits(nums1[i]);
         }
         if (size1 % 2){
             for (int j = 0; j < nums2.size(); j++) findSetBits(nums2[j]);
         }
+        int ans = 0;
         for (int i = 0; i < 31; i++) {
             if (bits[i] == 1) ans += pow(2, i);
         }
