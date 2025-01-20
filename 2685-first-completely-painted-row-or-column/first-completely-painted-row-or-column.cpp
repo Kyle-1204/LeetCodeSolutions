4580d0a4a37b53ae9bde4e6d6a57c725b263c71e
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-01-19 20:07:30 -0500

    Time: 108 ms (39.02%) | Memory: 170.5 MB (52.74%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
         unordered_map<int, pair<int, int>> indices;
-        int m = mat.size(), n = mat[0].size(), size = arr.size();
+        int m = mat.size(), n = mat[0].size();
         vector<int> rows(m), cols(n);
         for (int i = 0; i < m; i++){
             for (int j = 0; j < n; j++){
                 indices[mat[i][j]] = make_pair(i, j);
             }
         }
-        for (int i = 0; i < size; i++){
+        for (int i = 0; i < arr.size(); i++){
             int num = arr[i], row = indices[num].first, col = indices[num].second;
             rows[row]++;
             if (rows[row] == n) return i;
             cols[col]++;
             if (cols[col] == m) return i;
         }
-        return size;
+        return 0;
     }
 };
