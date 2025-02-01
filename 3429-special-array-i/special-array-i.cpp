c4df1b1617f807cd0d21ad81ca262c12c5047ed5
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-02-01 02:49:54 -0500

    Time: 0 ms (100.00%) | Memory: 27.6 MB (86.97%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
+        //iterate through indexes 0 to n - 1
         for (int i = 0; i < nums.size() - 1; i++){
-            if (nums[i] % 2 == nums[i+1] % 2) return false;
+
+            //compare the parities of the current and next element
+            if ((nums[i] & 1) ^ (nums[i+1] & 1) == 0) {
+
+                //if the two adjacent elements have the same parity, return false
+                return false;
+            }
         }
+
+        //return true if no pair of adjacent elements with the same parity are found
         return true;
     }
 };
