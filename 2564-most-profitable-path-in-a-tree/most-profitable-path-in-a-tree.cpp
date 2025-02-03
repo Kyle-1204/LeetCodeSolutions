05e8cec1dad706b072bf03ae5ceb5ad6fd423d07
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-02-03 18:25:25 -0500

    Time: 250 ms (27.99%) | Memory: 183.3 MB (25.16%) - LeetSync

--- 2564-most-profitable-path-in-a-tree/most-profitable-path-in-a-tree.cpp
+++ 2564-most-profitable-path-in-a-tree/most-profitable-path-in-a-tree.cpp
@@ -3,12 +3,19 @@ private:
     unordered_map<int, int> path;
     vector<bool> visited;
     vector<vector<int>> graph;
+
+    //Depth First Search
     bool DFS(int src, int time){
+        // Mark and set time node is reached
         path[src] = time;
         visited[src] = true;
+
+        // Destination for Bob is found
         if(src == 0){
             return true;
         }
+
+        // Traverse through unvisited nodes
         for(auto adj: graph[src]){
             if(!visited[adj]){
                 if(DFS(adj, time + 1)){
@@ -16,61 +23,59 @@ private:
                 }
             }
         }   
+        // If node 0 isn't reached, remove current node from path
         path.erase(src);
         return false;
     }
 
 public:
     int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount) {
-        int n = edges.size() + 1;
+        int n = edges.size() + 1, maxVal = INT_MIN;
         graph.resize(n);
         visited.assign(n, false);
-        for(auto it: edges){
-            graph[it[0]].push_back(it[1]);
-            graph[it[1]].push_back(it[0]);
+        queue<vector<int>> nodeQueue;
+        nodeQueue.push({0, 0, 0});
+
+        // Form tree with edges
+        for(auto edge: edges){
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
         }
         
-        // Find the path taken by bob to reach zero along with the time at which bob visited that node
+        // Find the path taken by bob to reach node 0 and the times it takes to get there
         DFS(bob, 0);
-        
-        // Push {src, time, income}
-        queue<vector<int>> q;
-        q.push({0, 0, 0});
+
+        //Breadth First Search
         visited.assign(n, false);
-                
-        int ans = INT_MIN;
-        while(!q.empty()){
-            int src = q.front()[0], time = q.front()[1], income = q.front()[2];
-            q.pop();
-            visited[src] = true;
-            
-            // If bob didn't visit this node
-            if(path.find(src) == path.end()){
+        while(!nodeQueue.empty()){
+            int src = nodeQueue.front()[0], time = nodeQueue.front()[1], income = nodeQueue.front()[2];
+
+            // Alice reaches the node first
+            if (path.find(src) == path.end() || time < path[src]) {
                 income += amount[src];
             }
-            else{
-                // Alice visits it first
-                if(time < path[src]){
-                    income += amount[src];
-                }
-                // Both visit at the same time
-                else if(time == path[src]){
-                    income += (amount[src]/2);
-                }
+
+            // Alice and Bob reach the node at the same time
+            else if (time == path[src]){
+                income += (amount[src]/2);
             }
-                        
-            // Updating when it is leaf
+       
+            // Update max value if current node is a leaf
             if(graph[src].size() == 1 && src != 0){
-                ans = max(ans, income);
+                maxVal = max(maxVal, income);
             }
-			// Exploring adjacent vertices
+			// Explore adjacent unvisited vertices
             for(auto adj: graph[src]){
                 if(!visited[adj]){
-                    q.push({adj, time+1, income});
+                    nodeQueue.push({adj, time+1, income});
                 }
             }
+            
+            // Mark and remove current node
+            visited[src] = true;
+            nodeQueue.pop();
         }
         
-        return ans;
+        return maxVal;
     }
 };
