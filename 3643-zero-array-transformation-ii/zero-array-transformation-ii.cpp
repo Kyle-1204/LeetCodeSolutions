e589ad5f128bfb9860f6460ae72757302f8f6130
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-02-16 18:10:39 -0500

    Time: 4 ms (92.53%) | Memory: 322.9 MB (93.92%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        int size = nums.size();
-        vector<int> count(size + 1);
-        int sum = 0, k = 0;
+        int n = nums.size(), sum = 0, k = 0;
+        vector<int> differenceArray(n + 1);
 
-        for (int index = 0; index < size; index++) {
-            while (sum + count[index] < nums[index]) {
+        for (int index = 0; index < n; index++) {
+            while (sum + differenceArray[index] < nums[index]) {
                 k++; // Increment k before accessing queries
-                if (k - 1 >= queries.size()) {
+                if (k > queries.size()) {
                     return -1;
                 }
                 int left = queries[k - 1][0], right = queries[k - 1][1], val = queries[k - 1][2];
                 if (right >= index) {
-                    count[max(left, index)] += val;
-                    count[right + 1] -= val;
+                    differenceArray[max(left, index)] += val;
+                    differenceArray[right + 1] -= val;
                 }
             }
-            sum += count[index];
+            sum += differenceArray[index];
         }
         return k;
     }
