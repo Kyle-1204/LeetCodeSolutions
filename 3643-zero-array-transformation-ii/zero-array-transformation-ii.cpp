281cb1d4b195d98e8af355823bc7246d44844376
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-02-16 23:46:01 -0500

    Time: 7 ms (86.33%) | Memory: 322.9 MB (86.96%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -1,43 +1,30 @@
 class Solution {
 public:
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size(), left = 0, right = queries.size();
-
-        // Zero array isn't formed after all queries are processed
-        if (!currentIndexZero(nums, queries, right)) return -1;
-        
-        // Binary Search
-        while (left <= right) {
-            int middle = left + (right - left) / 2;
-            if (currentIndexZero(nums, queries, middle)) {
-                right = middle - 1;
-            } else {
-                left = middle + 1;
-            }
-        }
-
-        //Return earliest query that zero array can be formed
-        return left;
-    }
-private:
-    bool currentIndexZero(vector<int>& nums, vector<vector<int>>& queries, int k){
-        int n = nums.size(), sum = 0;
+        int n = nums.size(), sum = 0, k = 0;
         vector<int> differenceArray(n + 1);
-
-        //Process query
-        for (int queryIndex = 0; queryIndex < k; queryIndex++) {
-            int start = queries[queryIndex][0], end = queries[queryIndex][1], val = queries[queryIndex][2];
-
-            //Process start and end of range
-            differenceArray[start] += val;
-            differenceArray[end + 1] -= val;
-        }
         
-        // Check if zero array can be formed
-        for (int numIndex = 0; numIndex < n; numIndex++) {
-            sum += differenceArray[numIndex];
-            if (sum < nums[numIndex]) return false;
+        //Iterate through nums
+        for (int index = 0; index < n; index++) {
+            //Iterate through queries while current index of nums cannot equal zero
+            while (sum + differenceArray[index] < nums[index]) {
+                k++;
+
+                // Zero array isn't formed after all queries are processed
+                if (k > queries.size()) {
+                    return -1;
+                }
+                int left = queries[k - 1][0], right = queries[k - 1][1], val = queries[k - 1][2];
+                
+                // Process start and end of range
+                if (right >= index) {
+                    differenceArray[max(left, index)] += val;
+                    differenceArray[right + 1] -= val;
+                }
+            }
+            //Update cumulative sum at current index
+            sum += differenceArray[index];
         }
-        return true;
+        return k;
     }
 };
