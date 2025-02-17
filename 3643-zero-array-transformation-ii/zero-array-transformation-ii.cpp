6859292ada3be5a6ab027085c7a6cec1b54695e8
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-02-16 23:08:44 -0500

    Time: 47 ms (70.51%) | Memory: 337.8 MB (65.82%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -3,9 +3,10 @@ public:
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size(), left = 0, right = queries.size();
 
-        //
+        // Zero array isn't formed after all queries are processed
         if (!currentIndexZero(nums, queries, right)) return -1;
         
+        // Binary Search
         while (left <= right) {
             int middle = left + (right - left) / 2;
             if (currentIndexZero(nums, queries, middle)) {
@@ -14,17 +15,25 @@ public:
                 left = middle + 1;
             }
         }
+
+        //Return earliest query that zero array can be formed
         return left;
     }
 private:
     bool currentIndexZero(vector<int>& nums, vector<vector<int>>& queries, int k){
         int n = nums.size(), sum = 0;
         vector<int> differenceArray(n + 1);
+
+        //Process query
         for (int queryIndex = 0; queryIndex < k; queryIndex++) {
             int start = queries[queryIndex][0], end = queries[queryIndex][1], val = queries[queryIndex][2];
+
+            //Process start and end of range
             differenceArray[start] += val;
             differenceArray[end + 1] -= val;
         }
+        
+        // Check if zero array can be formed
         for (int numIndex = 0; numIndex < n; numIndex++) {
             sum += differenceArray[numIndex];
             if (sum < nums[numIndex]) return false;
