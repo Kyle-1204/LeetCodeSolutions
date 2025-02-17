e07f563f4aabf37f92bd0c486d78c677b3350868
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-02-16 19:18:59 -0500

    Time: 1652 ms (7.59%) | Memory: 522.5 MB (7.34%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -1,7 +1,3 @@
-#include <vector>
-#include <iostream>
-using namespace std;
-
 class Solution {
 public:
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
@@ -23,12 +19,12 @@ public:
             return true;
         };
         
-        if (!check(queries.size())) return -1;
+        if (!currentIndexZero(nums, queries, queries.size())) return -1;
         
         int l = 0, r = queries.size();
         while (l <= r) {
             int m = l + (r - l) / 2;
-            if (check(m)) {
+            if (currentIndexZero(nums, queries, m)) {
                 r = m - 1;
             } else {
                 l = m + 1;
@@ -36,5 +32,22 @@ public:
         }
         
         return l;
+    }
+private:
+    bool currentIndexZero(vector<int> nums, vector<vector<int>> queries, int m){
+        int n = nums.size();
+        vector<int> arr(n + 1, 0);
+        for (int i = 0; i < m; ++i) {
+            int start = queries[i][0], end = queries[i][1], val = queries[i][2];
+            arr[start] += val;
+            if (end + 1 < n) arr[end + 1] -= val;
+        }
+            
+        int cur = 0;
+        for (int i = 0; i < n; ++i) {
+            cur += arr[i];
+            if (cur < nums[i]) return false;
+        }
+        return true;
     }
 };
