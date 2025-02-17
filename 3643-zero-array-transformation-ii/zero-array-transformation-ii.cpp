458fd07eaf12d5a42c6afecda6a5e34f3372a6d1
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-02-16 19:31:40 -0500

    Time: 41 ms (72.66%) | Memory: 337.8 MB (65.82%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -1,52 +1,32 @@
 class Solution {
 public:
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size();
+        int n = nums.size(), left = 0, right = queries.size();
+
+        if (!currentIndexZero(nums, queries, right)) return -1;
         
-        auto check = [&](int m) -> bool {
-            vector<int> arr(n + 1, 0);
-            for (int i = 0; i < m; ++i) {
-                int start = queries[i][0], end = queries[i][1], val = queries[i][2];
-                arr[start] += val;
-                if (end + 1 < n) arr[end + 1] -= val;
-            }
-            
-            int cur = 0;
-            for (int i = 0; i < n; ++i) {
-                cur += arr[i];
-                if (cur < nums[i]) return false;
-            }
-            return true;
-        };
-        
-        if (!currentIndexZero(nums, queries, queries.size())) return -1;
-        
-        int l = 0, r = queries.size();
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            if (currentIndexZero(nums, queries, m)) {
-                r = m - 1;
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (currentIndexZero(nums, queries, middle)) {
+                right = middle - 1;
             } else {
-                l = m + 1;
+                left = middle + 1;
             }
         }
-        
-        return l;
+        return left;
     }
 private:
     bool currentIndexZero(vector<int>& nums, vector<vector<int>>& queries, int m){
-        int n = nums.size();
-        vector<int> arr(n + 1, 0);
-        for (int i = 0; i < m; ++i) {
+        int n = nums.size(), sum = 0;
+        vector<int> differenceArray(n + 1, 0);
+        for (int i = 0; i < m; i++) {
             int start = queries[i][0], end = queries[i][1], val = queries[i][2];
-            arr[start] += val;
-            if (end + 1 < n) arr[end + 1] -= val;
+            differenceArray[start] += val;
+            if (end + 1 < n) differenceArray[end + 1] -= val;
         }
-            
-        int cur = 0;
         for (int i = 0; i < n; ++i) {
-            cur += arr[i];
-            if (cur < nums[i]) return false;
+            sum += differenceArray[i];
+            if (sum < nums[i]) return false;
         }
         return true;
     }
