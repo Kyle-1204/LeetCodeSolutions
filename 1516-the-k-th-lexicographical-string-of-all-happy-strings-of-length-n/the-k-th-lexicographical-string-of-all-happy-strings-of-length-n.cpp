965629ec1dec4b4a7fb196afb085277d7a21640b
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-02-19 11:36:59 -0500

    Time: 46 ms (16.75%) | Memory: 26.6 MB (19.27%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string getHappyString(int n, int k) {
+        generateStrings("", n);
+        if (strs.size() < k) return "";
+        sort(strs.begin(), strs.end());
+        return strs[k - 1];
+    }
+private:
+    vector<string> strs;
+    void generateStrings(string str, int n){
+        int len = str.length();
+        if (len == n){
+            strs.push_back(str);
+            return;
+        }
+        for (int i = 'a'; i < 'd'; i++){
+            char chr = char (i);
+            if (len == 0 || str.back() != chr) generateStrings(str + chr, n);
+        }
+    }
+};
