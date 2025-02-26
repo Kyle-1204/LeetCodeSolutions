8a24d2ca65afb6dfca38ffc97d4bf3af09334f0a
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-02-26 15:17:23 -0500

    Time: 0 ms (100.00%) | Memory: 45.1 MB (50.70%) - LeetSync

---
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxAbsoluteSum(vector<int>& nums) {
+        int minPrefixSum = 0, maxPrefixSum = 0, prefixSum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            prefixSum += nums[i];
+            minPrefixSum = min(minPrefixSum, prefixSum);
+            maxPrefixSum = max(maxPrefixSum, prefixSum);
+        }
+        return maxPrefixSum - minPrefixSum;
+    }
+};
