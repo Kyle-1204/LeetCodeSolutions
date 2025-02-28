0cd5b915f946ccc8f96da118d03765f06c781a0b
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-02-27 19:49:25 -0500

    Time: 0 ms (100.00%) | Memory: 26.9 MB (99.84%) - LeetSync

--- 3723-sum-of-good-numbers/sum-of-good-numbers.cpp
+++ 3723-sum-of-good-numbers/sum-of-good-numbers.cpp
@@ -3,8 +3,7 @@ public:
     int sumOfGoodNumbers(vector<int>& nums, int k) {
         int len = nums.size(), sum = 0;
         for (int i = 0; i < len; i++){
-            int num = nums[i];
-            if ((i - k < 0 || num > nums[i - k]) && (i + k >= len || num > nums[i + k])) sum += num;
+            if ((i - k < 0 || nums[i] > nums[i - k]) && (i + k >= len || nums[i] > nums[i + k])) sum += nums[i];
         }
         return sum;
     }
