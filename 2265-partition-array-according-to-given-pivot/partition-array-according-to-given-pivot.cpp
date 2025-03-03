db1d1dac7713fc0ec4173b7e83699134ef806459
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-03-02 19:17:49 -0500

    Time: 7 ms (59.22%) | Memory: 141.6 MB (7.79%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<vector<int>> vects(3);
-        vector<int> ans;
+        vector<int> lessers, equals, greaters;
         for (auto& num: nums){
-            if (num < pivot) vects[0].push_back(num);
-            else if (num > pivot) vects[2].push_back(num);
-            else vects[1].push_back(num);
+            if (num < pivot) lessers.push_back(num);
+            else if (num > pivot) greaters.push_back(num);
+            else equals.push_back(num);
         }
-        for (auto& vect: vects){
-            for (auto& num: vect) ans.push_back(num);
-        }
-        return ans;
+        for (auto& equal: equals) lessers.push_back(equal);
+        for (auto& greater: greaters) lessers.push_back(greater);
+        return lessers;
     }
 };
