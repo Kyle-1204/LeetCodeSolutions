f8d957c8b17aaeff5ff572a202ba83577e2946dc
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-03-04 15:54:32 -0500

    Time: 202 ms (11.04%) | Memory: 160 MB (10.41%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
-        //vector<int> vect(days + 1);
         map<int, int> dayMap;
         int prefixSum = 0, freeDays = 0, previousDay = days;
         bool hasGap = false;
 
-        for (auto& meeting: meetings){
+        for (auto& meeting : meetings) {
             // Set first day of meetings
             previousDay = min(previousDay, meeting[0]);
 
-            //Process start and end of meeting
+            // Process start and end of meeting
             dayMap[meeting[0]]++;
             dayMap[meeting[1] + 1]--;
         }
 
-        //Add all days before the first day of meetings
+        // Add all days before the first day of meetings
         freeDays += (previousDay - 1);
-        for (auto& day: dayMap){
-
-            //Add current range of days without a meeting
-            if (hasGap){
-                freeDays += day.first - previousDay;
-                hasGap = false;
+        for (auto& day : dayMap) {
+            int currentDay = day.first;
+            // Add current range of days without a meeting
+            if (prefixSum == 0) {
+                freeDays += currentDay - previousDay;
             }
             prefixSum += day.second;
-
-            //No meetings at the current day
-            if (prefixSum == 0) {
-                hasGap = true;
-            }
-            previousDay = day.first;
+            previousDay = currentDay;
         }
 
-        //Add all days after the last day of meetings
+        // Add all days after the last day of meetings
         freeDays += days - previousDay + 1;
         return freeDays;
     }
