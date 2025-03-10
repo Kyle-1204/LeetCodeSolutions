8a441f535662bd97b0620c22a40e1e242e115e9a
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-03-10 12:50:07 -0400

    Time: 3 ms (36.82%) | Memory: 9.1 MB (36.86%) - LeetSync

--- 3736-find-valid-pair-of-adjacent-digits-in-string/find-valid-pair-of-adjacent-digits-in-string.cpp
+++ 3736-find-valid-pair-of-adjacent-digits-in-string/find-valid-pair-of-adjacent-digits-in-string.cpp
@@ -5,7 +5,8 @@ public:
         unordered_map<char, int> mp;
         for (int i = 0; i < len; i++) mp[s[i]]++;
         for (int j = 0; j < len - 1; j++){
-            if (s[j] != s[j+1] && mp[s[j]] == s[j] - '0' && mp[s[j+1]] == s[j+1] - '0') return s.substr(j, 2);
+            char first = s[j], second = s[j+1];
+            if (first != second && mp[first] == first - '0' && mp[second] == second - '0') return s.substr(j, 2);
         }
         return "";
     }
