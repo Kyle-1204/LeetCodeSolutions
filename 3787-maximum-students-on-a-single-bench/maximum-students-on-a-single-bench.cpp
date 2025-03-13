15edf441f07800376fef83e1c318bebfd31e1b30
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-03-13 18:52:32 -0400

    Time: 17 ms (46.55%) | Memory: 54.1 MB (47.13%) - LeetSync

--- 3787-maximum-students-on-a-single-bench/maximum-students-on-a-single-bench.cpp
+++ 3787-maximum-students-on-a-single-bench/maximum-students-on-a-single-bench.cpp
@@ -4,8 +4,9 @@ public:
         unordered_map<int, unordered_set<int>> mp;
         int maxStudents = 0;
         for (auto& student: students){
-            mp[student[1]].insert(student[0]);
-            maxStudents = max(maxStudents, (int) mp[student[1]].size());
+            int bench = student[1];
+            mp[bench].insert(student[0]);
+            maxStudents = max(maxStudents, (int) mp[bench].size());
         }
         return maxStudents;
     }
