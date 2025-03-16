bb7536aa5afd4161f246aa8784a0d51d09356bc9
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-03-16 01:55:26 -0400

    Time: 2164 ms (5.09%) | Memory: 70.7 MB (5.08%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -4,14 +4,12 @@ public:
     long long repairCars(vector<int>& ranks, int cars) {
         priority_queue<pi, vector<pi>, greater<pi>> pq;
         for (auto& rank: ranks) pq.push(make_pair(rank, make_pair(rank, 1)));
-        long long ans = 0;
-        while (cars--){
+        while (--cars){
             int r = pq.top().second.first, n = pq.top().second.second;
-            ans = pq.top().first;
             n++;
             pq.push(make_pair((long long) (r) * n * n, make_pair(r, n)));
             pq.pop();
         }
-        return ans;
+        return pq.top().first;
     }
 };
