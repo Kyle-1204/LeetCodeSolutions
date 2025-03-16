14354c249851c014dfee94ccb3e3e213ccd43352
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-03-16 01:54:37 -0400

    Time: 2127 ms (5.09%) | Memory: 70.8 MB (5.08%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
-    typedef pair<long long, pair<int, int>> pi;
+    typedef pair<long long, pair<int, int>> pi; //(time, (r,n))
     long long repairCars(vector<int>& ranks, int cars) {
         priority_queue<pi, vector<pi>, greater<pi>> pq;
-        for (auto& rank: ranks){
-            pq.push(make_pair((long long) rank, make_pair(rank, 1)));
-        }
-        //cars++;
+        for (auto& rank: ranks) pq.push(make_pair(rank, make_pair(rank, 1)));
         long long ans = 0;
         while (cars--){
-            int rank = pq.top().second.first, mins = pq.top().second.second;
-            long long time = pq.top().first;
-            //cout << time << endl;
-            ans = max(ans, time);
-            mins++;
-            //time = (long long) (rank) * mins * mins;
-            pq.push(make_pair((long long) (rank) * mins * mins, make_pair(rank, mins)));
-            pq.pop();
-        }
-        /*
-        long long ans = 0;
-        while (!pq.empty()) {
+            int r = pq.top().second.first, n = pq.top().second.second;
             ans = pq.top().first;
-            cout << ans << endl;
+            n++;
+            pq.push(make_pair((long long) (r) * n * n, make_pair(r, n)));
             pq.pop();
         }
-        */
         return ans;
     }
 };
