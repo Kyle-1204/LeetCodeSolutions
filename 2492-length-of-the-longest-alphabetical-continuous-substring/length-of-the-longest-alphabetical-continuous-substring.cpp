2e4968bc05e89d7f9aa93be767daad96758407d5
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-03-17 17:25:26 -0400

    Time: 6 ms (38.75%) | Memory: 18.1 MB (18.24%) - LeetSync

---
+++ 2492-length-of-the-longest-alphabetical-continuous-substring/length-of-the-longest-alphabetical-continuous-substring.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int longestContinuousSubstring(string s) {
+        int ans = 1, currLen = 1;
+        char currChar = ' ';
+        for (auto& chr: s){
+            if (chr - currChar == 1){
+                currLen++;
+                ans = max(currLen, ans);
+            }
+            else currLen = 1;
+            currChar = chr;
+        }
+        return ans;
+    }
+};
