00b79e29694ca8aa4b277e13d7d9c05042e3526b
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-03-17 16:49:58 -0400

    Time: 0 ms (100.00%) | Memory: 63 MB (76.53%) - LeetSync

--- 2354-minimum-health-to-beat-game/minimum-health-to-beat-game.cpp
+++ 2354-minimum-health-to-beat-game/minimum-health-to-beat-game.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     long long minimumHealth(vector<int>& damage, int armor) {
-        return accumulate(damage.begin(), damage.end(), 0LL) - min(armor, *max_element(damage.begin(), damage.end())) + 1;
+        vector<int>::iterator bgn = damage.begin(), end = damage.end();
+        return accumulate(bgn, end, 0LL) - min(armor, *max_element(bgn, end)) + 1;
     }
 };
