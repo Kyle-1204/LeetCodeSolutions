5cf8971b9d4e8989af1746f157bb85700156784d
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-03-17 01:07:34 -0400

    Time: 4 ms (14.29%) | Memory: 29.7 MB (0.00%) - LeetSync

--- 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.cpp
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int maxSum(vector<int>& nums) {
+        int sum = 0, maxVal = *max_element(nums.begin(), nums.end());
+        if (maxVal <= 0) return maxVal;
         unordered_set<int> vals;
-        int sum = 0;
         for (auto& num: nums){
             if (num > 0 && vals.find(num) == vals.end()){
                 sum += num;
                 vals.insert(num);
             }
         }
-        return (sum != 0 ? sum : *max_element(nums.begin(), nums.end()));
+        return sum;
     }
 };
