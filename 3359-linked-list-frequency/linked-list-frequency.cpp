aac8616ded4d7b342b6b5927d1ef844fcbe30a9e
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-03-22 23:29:23 -0400

    Time: 8 ms (99.17%) | Memory: 212.4 MB (59.17%) - LeetSync

---
+++ 3359-linked-list-frequency/linked-list-frequency.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* frequenciesOfElements(ListNode* head) {
+        unordered_map<int, int> mp;
+        while (head != NULL){
+            mp[head->val]++;
+            head = head->next;
+        }
+        ListNode* node = new ListNode(0);
+        head = node;
+        for (auto& val: mp){
+            node->next = new ListNode(val.second);
+            node = node->next;
+        }
+        return head->next;
+    }
+};
