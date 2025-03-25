9c259b533504b1450189a6292accc45653f8ea3c
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-03-25 13:01:06 -0400

    Time: 8 ms (48.82%) | Memory: 31.8 MB (28.35%) - LeetSync

--- 3796-longest-common-prefix-after-at-most-one-removal/longest-common-prefix-after-at-most-one-removal.cpp
+++ 3796-longest-common-prefix-after-at-most-one-removal/longest-common-prefix-after-at-most-one-removal.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int longestCommonPrefix(string s, string t) {
-        bool removeFree = true;
         int si = 0, ti = 0, len = s.length();
         while (si < len){
             if (s[si] != t[ti]){
-                if (removeFree && si < len - 1 && s[si + 1] == t[ti]){
-                    si++;
-                    removeFree = false;
-                }
+                if (si == ti && si < len - 1 && s[si + 1] == t[ti]) si++;
                 else return ti;
             }
             si++;
