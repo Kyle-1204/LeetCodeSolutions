26e6b00d786805c274286c9c4fac6b5221ab42ff
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-04-26 02:08:07 -0400

    Time: 428 ms (67.84%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-SELECT FirstName, LastName, City, State
-from Person left join Address
-on Person.PersonId = Address.PersonId
+SELECT FirstName, LastName, City, State # Select the required columns from both tables
+from Person left join Address # Merge the Person table with the Address table on personID
+on Person.PersonId = Address.PersonId # Return everyone from the person table - if no matching address, city/state will be NULL
