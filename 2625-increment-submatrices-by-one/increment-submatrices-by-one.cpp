11d0464d05e37a16f2592d4d4c9c98841e035836
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2025-11-13 19:25:56 -0500

    Time: 1674 ms (5.33%) | Memory: 86.3 MB (99.06%) - LeetSync

---
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        vector<vector<int>> matrix(n, vector<int>(n, 0));
+        for (auto& query: queries){
+            for (int i = query[0]; i <= query[2]; i++){
+                for (int j = query[1]; j <= query[3]; j++){
+                    matrix[i][j]++;
+                }
+            }
+        }
+        return matrix;
+    }
+};
