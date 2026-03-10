cd91b6fcff6800c30d17c20cd1f632fac3f12deb
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2026-03-09 22:18:57 -0400

    Time: 0 ms (100.00%) | Memory: 23.3 MB (69.35%) - LeetSync

--- 4247-minimum-capacity-box/minimum-capacity-box.cpp
+++ 4247-minimum-capacity-box/minimum-capacity-box.cpp
@@ -3,13 +3,7 @@ public:
     int minimumIndex(vector<int>& capacity, int itemSize) {
         int minIndex = -1, minCapacity = INT_MAX;
         for (int i = 0; i < capacity.size(); i++){
-            if (minIndex == -1){
-                if (capacity[i] >= itemSize){
-                    minIndex = i;
-                    minCapacity = capacity[i];
-                }
-            }
-            else if (capacity[i] >= itemSize && capacity[i] < minCapacity){
+            if (capacity[i] >= itemSize && capacity[i] < minCapacity){
                 minIndex = i;
                 minCapacity = capacity[i];
             }
