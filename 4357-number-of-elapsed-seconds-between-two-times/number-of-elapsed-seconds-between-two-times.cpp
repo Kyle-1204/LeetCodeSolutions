8875ac7112aef48e0f92c47f24c57d5f08e1846a
Kyle Enriquez <70853997+Kyle-1204@users.noreply.github.com>
2026-07-14 16:40:18 -0400

    Time: 0 ms (100.00%) | Memory: 9 MB (92.92%) - LeetSync

--- 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.cpp
+++ 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int secondsBetweenTimes(string startTime, string endTime) {
-        int start = 0, end = 0;
-        start += startTime[7] - '0';
-        end += endTime[7] - '0';
-        start += (startTime[6] - '0') * 10;
-        end += (endTime[6] - '0') * 10;
-        start += (startTime[4] - '0') * 60;
-        end += (endTime[4] - '0') * 60;
-        start += (startTime[3] - '0') * 600;
-        end += (endTime[3] - '0') * 600;
-        start += (startTime[1] - '0') * 3600;
-        end += (endTime[1] - '0') * 3600;
-        start += (startTime[0] - '0') * 36000;
-        end += (endTime[0] - '0') * 36000;
+        int start = 0, end = 0, mult = 1;
+        bool isSecondDigit = true;
+        for (int i = 7; i >= 0; i--){
+            start += (startTime[i] - '0') * mult;
+            end += (endTime[i] - '0') * mult;
+            if (isSecondDigit) mult *= 10;
+            else{
+                mult *= 6;
+                i--;
+            }
+            isSecondDigit = !isSecondDigit;
+        }
         return end - start;
     }
 };
